fd7a9fb99ca9b1901ec26c443a7ddde9bc923481
azagorny <azagorny@cnord.ru>
2014-11-11 14:58:00 +0300

    Версия конфигов без cask

---
+++ .cask/24.3.1/bootstrap/git-20140128.241/git-autoloads.el
@@ -0,0 +1,18 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("git-pkg.el" "git.el") (21601 63875 65946
+;;;;;;  347000))
+
+;;;***
+
+(provide 'git-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-autoloads.el ends here
---
+++ .cask/24.3.1/bootstrap/s-20140910.334/s-autoloads.el
@@ -0,0 +1,18 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21601 63872 942382
+;;;;;;  674000))
+
+;;;***
+
+(provide 's-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; s-autoloads.el ends here
