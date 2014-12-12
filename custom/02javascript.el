d23a3557093f6e2d3039a168a9800df6fc39e4c6
azagorny <azagorny@cnord.ru>
2014-12-12 15:28:30 +0300

    Дописываю для яваскрипта конфиг

--- custom/02javascript.el
+++ custom/02javascript.el
@@ -1,3 +1,5 @@
 (add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js-mode-hook 'linum-mode)
+(add-hook 'js-mode-hook 'show-paren-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 (setq js2-highlight-level 3)
